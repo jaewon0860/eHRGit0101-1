b1393d140c5b8413bbfb88eede8ea75efb1cb46f
sist <sist@DESKTOP-MV39HJ7>
2020-04-07 09:51:58 +0900

    init

---
+++ eHRGit0101/src/eHRGit0101/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package eHRGit0101;
+
+/**
+ * @author sist
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, Git.");
+
+	}
+
+}
